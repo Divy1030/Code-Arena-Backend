de0db9e676b605b41c2d00d1c63d122875ef2d7e
vansh mac <vansh23164033@akgec.ac.in>
2026-01-30 17:56:29 +0530

    fix: remove unused imports and streamline problem routes

--- src/routes/problem.routes.ts
+++ src/routes/problem.routes.ts
@@ -1,14 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
-import {
-  getProblem,
-  submitSolution,
-  getLeaderboard,
-} from "../controllers/problem.controllers.js";
-
 import { getContestProblems } from "../controllers/contest.controllers.js";
-<<<<<<< HEAD
 import { submitSolution, getProblem, getLeaderboard, getAllProblems } from "../controllers/problem.controllers.js";
 
 const router = Router();
@@ -17,24 +9,5 @@ router.route('/submit-solution/:contestId/:problemId').post(verifyJWT, submitSol
 router.route('/get-problem/:contestId/:problemId').get(verifyJWT, getProblem);
 router.route('/get-leaderboard/:contestId').get(verifyJWT, getLeaderboard);
 router.route('/get-all-problems').get(verifyJWT, getAllProblems);
-=======
 
-const router = Router();
-
-// Submit solution for a problem
-router
-  .route("/submit-solution/:contestId/:problemId")
-  .post(verifyJWT, submitSolution);
->>>>>>> 6e81763c3af700dfb5416e6e1692c6a14c4859d0
-
-// Get a specific problem
-router
-  .route("/get-problem/:contestId/:problemId")
-  .get(verifyJWT, getProblem);
-
-// Get leaderboard for a contest
-router
-  .route("/get-leaderboard/:contestId")
-  .get(verifyJWT, getLeaderboard);
-
-export { router as problemRouter };
+export default router;
